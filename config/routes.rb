2f1ac1a6a2981f56858d15ba1bb7c9cf9755af28
Peter3452 <1312402@gmail.com>
2022-10-11 16:51:12 +0300

    add scaffold Product & converted image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :products
   
   get 'welcome', to: 'welcome#index'
-  root to: 'welcome#index'
+  root to: 'products#index'
 
   get 'sign_up', to: 'users#new'
   post 'sign_up', to: 'users#create' #для создания пользователя
